6fef5887fc2a56eb059f976f1477f05e3db973d8
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-04-22 19:53:36 -0500

    Validando crear un producto en el servidor

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express"
+import {check, validationResult} from 'express-validator'
 import Product  from "../models/Product.model"
 
 export const createProduct = async (req: Request, res: Response) => {
+
+  await check('name', 'El nombre del producto es obligatorio')
+        .notEmpty().run(req)
+
+  await check('price')
+        .isNumeric().withMessage('El precio debe ser un numero')
+        .notEmpty().withMessage('El precio es obligatorio')
+        .custom((value) => value > 0).withMessage('El precio debe ser mayor a 0')
+        .run(req)
+        
+  let errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
 
  const product = await Product.create(req.body)
     res.json({ data: product })
