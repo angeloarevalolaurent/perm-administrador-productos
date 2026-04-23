a642e6d1160ff3ff3b2071d3755c1a97fb8655d0
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-04-23 11:43:13 -0500

    Probando validacion de crear Producto

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,17 @@ import request from 'supertest'
 import server from "../../server";
 
 describe('POST /api/products', () => {
+    // Prueba de validación de entrada
+    it('should display validation errors for invalid input', async () => {
+        const response = await request(server)
+            .post('/api/products')
+            .send({ })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('error')
+
+    })
+// Prueba de creación exitosa
     it('should create a new product', async () => {
         const response = await request(server)
             .post('/api/products')
