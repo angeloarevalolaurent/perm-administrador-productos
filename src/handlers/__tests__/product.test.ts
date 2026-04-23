ce484e044cca8898bc8b809be468dd75595723cb
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-04-23 11:49:50 -0500

    Validando que el precio sea mayor a 0

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -9,9 +9,34 @@ describe('POST /api/products', () => {
             .send({ })
 
         expect(response.status).toBe(400)
-        expect(response.body).toHaveProperty('error')
+        expect(response.body).toHaveProperty('errors')
 
     })
+
+
+    it('should validate that the price is greater than 0', async () => {
+        const response = await request(server)
+            .post('/api/products')
+            .send({ 
+                name: 'Audifonos',
+                price: -10
+             })
+          
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+
+    })
+
+
+
+
+
+
+
+
+
+
 // Prueba de creación exitosa
     it('should create a new product', async () => {
         const response = await request(server)
